db9bfca4c76bf8960b2b90c43416e20aee8a188d
tmastrom <thomas.mastromonaco@gmail.com>
2026-04-02 20:10:16 -0700

    workflow to deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://tmastrom.github.io', 
+    base: '/tmastrom.github.io',
+});
