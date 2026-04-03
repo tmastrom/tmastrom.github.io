b8dca246612c6954a43a320c59307c6c95149678
tmastrom <thomas.mastromonaco@gmail.com>
2026-04-02 20:16:56 -0700

    fix css asset directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://tmastrom.github.io', 
-    base: '/tmastrom.github.io',
+    base: '/',
 });
