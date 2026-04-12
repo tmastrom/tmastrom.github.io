f17fd41a41c2259a5faafa7efe23fef811db8953
tmastrom <thomas.mastromonaco@gmail.com>
2026-04-12 16:46:21 -0700

    biome linting and formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,22 @@
 import { defineConfig, fontProviders } from "astro/config";
 
 export default defineConfig({
-	site: "https://tmastrom.github.io",
 	base: "/",
 	fonts: [
 		{
-			provider: fontProviders.local(),
-			name: "Satoshi",
 			cssVariable: "--font-satoshi",
+			name: "Satoshi",
 			options: {
 				variants: [
 					{
 						src: ["./src/assets/fonts/Satoshi-Variable.woff2"],
-						weight: "100 900",
 						style: "normal",
-					},
-				],
+						weight: "100 900"
+					}
+				]
 			},
-		},
+			provider: fontProviders.local()
+		}
 	],
+	site: "https://tmastrom.github.io"
 });
